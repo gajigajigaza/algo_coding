20c8dfc36b926e238e2fe1101e88d355166af835
gajigajigaza <127067635+gajigajigaza@users.noreply.github.com>
2026-02-11 18:27:14 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,26 +2,29 @@ import sys
 from collections import deque
 
 def dfs(ground): #ground는 배추 2차원 배열
-    visited = []
     need_visited = []
     cnt = 0
+
+    dx = [0, 0, -1, 1] # 방향 
+    dy = [1, -1, 0, 0]
+
     for i in range (n):
         for j in range (m):
-            num = ground[i][j]
-            if  num == 1 and (i, j) not in visited:
-                need_visited.append((i, j))
+            if ground[i][j] == 1:
+                ground[i][j] = 0
+                need_visited.append((i,j))
                 cnt += 1
+
                 while need_visited:
-                    c =(need_visited.pop())
-                    visited.append(c)
-                    if c[0]+1 < n and ground[c[0]+1][c[1]] == 1 and (c[0]+1, c[1]) not in visited:
-                        need_visited.append((c[0]+1, c[1]))
-                    if c[1]+1 < m and ground[c[0]][c[1]+1] == 1 and (c[0], c[1]+1) not in visited:
-                        need_visited.append((c[0], c[1]+1))
-                    if 0 <= c[0]-1 and ground[c[0]-1][c[1]] == 1 and (c[0]-1, c[1]) not in visited:
-                        need_visited.append((c[0]-1, c[1]))
-                    if 0 <= c[1]-1 and ground[c[0]][c[1]-1] == 1 and (c[0], c[1]-1) not in visited:
-                        need_visited.append((c[0], c[1]-1))
+                    cx, cy = need_visited.pop()
+
+                    for k in range(4): # 방향
+                        nx = cx + dx[k]
+                        ny = cy + dy[k]
+
+                        if 0 <= nx < n and 0 <= ny < m and ground[nx][ny] == 1:
+                            ground[nx][ny] = 0 # 방문 처리
+                            need_visited.append((nx, ny))           
 
     return cnt
 
